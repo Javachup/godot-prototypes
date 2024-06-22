3a9fcbc06e641cf74dee6194c2efbd1728cf7ce4
Joey Romanowski <javachup@gmail.com>
2024-06-22 10:03:09 -0400

    Adds a button to the track

--- RhythmPrototype/Scripts/track.gd
+++ RhythmPrototype/Scripts/track.gd
@@ -1,12 +1,18 @@
 class_name Track
 extends Path2D
 
+@onready var button = %Button
+
 @export var note:PackedScene
+@export_range(0,1) var buttonPosition := 0.9
 
 var notes:Array[Note] = []
 
+func _ready():
+	button.progress_ratio = buttonPosition
+
 func spawn_note():
 	var temp = note.instantiate() as Note
-	temp.path_length = curve.get_baked_length()
+	temp.path_length = curve.get_baked_length() * buttonPosition
 	notes.append(temp)
 	add_child(temp)
