078af87a9810638009b4fb886a21fff514d4ab20
Joey Romanowski <javachup@gmail.com>
2024-06-30 14:23:28 -0400

    adds song loading dialog

--- RhythmPrototype/Scripts/song.gd
+++ RhythmPrototype/Scripts/song.gd
@@ -1,6 +1,7 @@
 class_name Song
 extends Resource
 
+@export var name:String
 @export var bpm := 100
 @export var beats_per_measure := 4
 @export var start_delay := 0.0
--- RhythmPrototype/addons/song_maker/song_maker_dock.gd
+++ RhythmPrototype/addons/song_maker/song_maker_dock.gd
@@ -7,6 +7,11 @@ var track_data:Array[TrackData]
 @onready var track_names_parent = %TrackNames
 @onready var track_data_parent = %TrackData
 
+@onready var file_dialog = %FileDialog
+@onready var song_name_edit = %SongNameEdit
+
+var song:Song
+
 func _ready():
 	# Register the already existing nodes
 	for child in track_names_parent.get_children():
@@ -23,9 +28,24 @@ func _ready():
 		track_names[i].custom_minimum_size.y = size
 		track_data[i].custom_minimum_size.y = size
 
+	# Set up file dialogue
+	file_dialog.current_dir = "res://Resources/Songs"
+	file_dialog.add_filter("*.tres", "Song Resources")
+
 func _on_save_button_pressed():
 	save()
 
 func save():
 	print("Save!")
-	pass
+
+func _on_load_song_pressed():
+	file_dialog.popup()
+
+func _on_file_dialog_file_selected(path):
+	var temp = load(path)
+	if !(temp is Song):
+		printerr("File must be a song resource!")
+		return
+	song = temp as Song
+
+	song_name_edit.text = song.name
